4cf02ced0457f192c0a40c218fca9abe4758b957
olivrz <oliver.rzep@gmail.com>
2022-09-15 19:58:30 -0400

    Update style

--- src/app/App.js
+++ src/app/App.js
@@ -5,19 +5,25 @@ import React from "react";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 //Import components
+import NavBar from '../components/NavBar';
+import SiteSection from '../components/SiteSection';
 import Header from '../components/Header';
 
 function App() {
   return (
-      <Router>
+      <div>
           <Header />
+      <Router>
+          <NavBar />
           <main>
               <Routes>
 
               </Routes>
           </main>
 
-    </Router>
+        </Router>
+        <SiteSection name='about-me'/>
+      </div>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,8 @@
-import React from "react";
+import React from 'react';
 
-// Import the NavLink component.
-import {NavLink} from 'react-router-dom';
+export default function Header() {
 
-export default function Header () {
-
-    return (
-        <div className="header">
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/articles">Articles</NavLink>
-            <NavLink to="/categories">Categories</NavLink>
-        </div>
+    return(
+        <h1 className="header">Oliver Rzepecki</h1>
     )
 }
