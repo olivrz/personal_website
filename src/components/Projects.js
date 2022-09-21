4ab3a891121a6eedb39a98038080173eadf9d10b
olivrz <oliver.rzep@gmail.com>
2022-09-21 18:35:06 -0400

    Update projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,21 +4,29 @@ import pic1 from '../images/nyc_skyline.jpg';
 
 export default function Projects() {
     return (
-        <div className="row">
+        <div className="row picture-grid">
             <div className="pictureBox">
-                <img src={pic1} className="picture"/>
+                <div className="projects picture-holder">
+                    <img src={pic1} className="picture"/>
+                </div>
                 <h6 className="project-title">Project 1 title</h6>
             </div>
             <div className="pictureBox">
-                <img src={pic1} className="picture"/>
+                <div className="projects picture-holder">
+                    <img src={pic1} className="picture"/>
+                </div>
                 <h6 className="project-title">Project 2 title</h6>
             </div>
             <div className="pictureBox">
-                <img src={pic1} className="picture"/>
+                <div className="projects picture-holder">
+                    <img src={pic1} className="picture"/>
+                </div>
                 <h6 className="project-title">Project 3 title</h6>
             </div>
             <div className="pictureBox">
-                <img src={pic1} className="picture"/>
+                <div className="projects picture-holder">
+                    <img src={pic1} className="picture"/>
+                </div>
                 <h6 className="project-title">Project 4 title</h6>
             </div>
         </div>
