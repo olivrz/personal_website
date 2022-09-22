39232eaa145d485c11a423bc6484e6376127735d
olivrz <oliver.rzep@gmail.com>
2022-09-22 13:28:39 -0400

    Add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <div className="footer flex-container background-gray">
+            <h2 className="section-title">Contact</h2>
+            <div className="row">
+                
+            </div>
+        </div>
+    )
+}
